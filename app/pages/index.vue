c17b6b49eb6915e2af6eaa09706988f73b36619f
quoctrang <quoctrang2701@gmail.com>
2025-11-30 13:23:27 +0700

    add font

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <h1>Toeic Web App</h1>
+    <h1>Learn English with AI</h1>
+    <nav>
+      <NuxtLink to="/toeic">toeic</NuxtLink>
+    </nav>
   </div>
 </template>
+
+<script setup lang="ts">
+// const { data } = await useFetch('http://localhost:8080/api/words')
+// const { data: part5 } = await useFetch('http://localhost:8080/api/part5')
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  modules: ['@nuxtjs/google-fonts', '@nuxtjs/tailwindcss'],
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    }
+  },
+  css: [
+    '@/assets/css/main.css'
+  ]
 })
